13fc84fc11b5963f61a2311c555ea641ec85aa57
keyrrae <xuan-wang@outlook.com>
2016-05-18 17:14:24 -0700

    client rewritten

--- raft/client.rb
+++ raft/client.rb
@@ -1,6 +1,59 @@
 require 'bunny'
 
 class Client
+  attr_accessor(:request_queue)
+
+  def initialize(ip, queue_name)
+    @conn = Bunny.new(:hostname => ip)
+    @conn.start
+    @ch   = @conn.create_channel
+    self.request_queue = Quene.new
+    @leader = None
+    @servers = []
+    @msg_queue    = @ch.queue(queue_name)
+    @cmd = CommandInterface.new(self)
+    run
+  end
+
+  def run
+    # Producer
+    t1 = Thread.new do
+      # Start command line
+      @cmd.run
+    end
+
+
+    # Consumer
+    t2 = Thread.new do
+      # TODO: client protocol
+    end
+
+    t1.join
+    t2.join
+
+  end
+
+  def push_request(request)
+    self.request_queue << request
+  end
+
+  def pop_request
+    if self.request_queue.empty?
+      None
+    elsif
+      req << self.request_queue
+      req
+    end
+  end
+
+
+end
+
+class CommandInterface
+  def initialize(client_context)
+    @client = client_context
+  end
+
   def print_commands
     puts '================================================='
     puts 'post(p) <message>'
@@ -17,56 +70,65 @@ class Client
     STDOUT.flush
   end
 
-  def initialize(ip, queue_name)
-    @conn = Bunny.new(:hostname => ip)
-    @conn.start
-    @ch   = @conn.create_channel
-    @msg_queue    = @ch.queue(queue_name)
-  end
 
   def run
     print_commands
 
     print '> '
-    t = Thread.new do
-      while true
-        cmd = gets
-        cmd_parsed = cmd.strip.split(' ', 2)
+    STDOUT.flush
 
-        if cmd.match("^\n$")
-          print '> '
-          next
-        end
+    while true
+      cmd = gets
+      cmd_parsed = cmd.strip.split(' ', 2)
 
-        case cmd_parsed[0]
-          when 'post' , 'p'
-            if cmd_parsed.length != 2
-              puts 'Empty message'
-            else
-              puts 'Posting message: ' + cmd_parsed[1]
-              # TODO: post message
-              @ch.default_exchange.publish(cmd_parsed[1], :routing_key => @msg_queue.name)
-            end
-
-          when 'lookup' , 'l'
-            puts 'Looking up'
-          # TODO: look up function
-
-          when 'exit' , 'e', 'quit', 'q'
-            puts 'Exiting'
-            exit(0)
-
-          when 'help', 'h'
-            print_commands
-          else
-            puts 'Wrong command, type "help" or "h" for help.'
-        end
+      if cmd.match("^\n$")
         print '> '
+        STDOUT.flush
+        next
       end
+
+      case cmd_parsed[0]
+        when 'post' , 'p'
+          if cmd_parsed.length != 2
+            puts 'Empty message'
+          else
+            puts 'Posting message: ' + cmd_parsed[1]
+            # TODO: post message
+            @ch.default_exchange.publish(cmd_parsed[1], :routing_key => @msg_queue.name)
+          end
+
+        when 'lookup' , 'l'
+          puts 'Looking up'
+        # TODO: look up function
+
+        when 'exit' , 'e', 'quit', 'q'
+          puts 'Exiting'
+          exit(0)
+
+        when 'help', 'h'
+          print_commands
+        else
+          puts 'Wrong command, type "help" or "h" for help.'
+      end
+      print '> '
+      STDOUT.flush
     end
-    t.join
-    @con.close
+
   end
+
+end
+
+
+class ClientRequest
+
+end
+
+class LookupRequest < ClientRequest
+
+end
+
+class PostRequest < ClientRequest
+
 end
 
 c = Client.new('169.231.10.109', 'hello')
